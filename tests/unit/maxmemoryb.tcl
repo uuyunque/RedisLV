7eb1d790d4766c4d1adf8cce84aeccf70880594a
uuyunque <65013725@qq.com>
2018-02-06 16:11:13 +0800

    增加测试用例

--- tests/unit/maxmemoryb.tcl
+++ tests/unit/maxmemoryb.tcl
@@ -3,16 +3,17 @@ start_server {tags {"maxmemoryb"}} {
         r flushall
         r config set maxmemory-policy noeviction 
         set numkeys 1 
-	set veri_set_key "" 
-	set veri_set_val "" 
-	set veri_sadd_key "" 
-        set veri_sadd_val "" 
-	set veri_hash_key "" 
-	set veri_mem_key ""
-	set veri_hash_val "" 
-	set veri_score "" 
-	set veri_zadd_key "" 
-	set veri_zadd_mem "" 
+        set numerr 0 
+		set veri_set_key "" 
+		set veri_set_val "" 
+		set veri_sadd_key "" 
+		set veri_sadd_val "" 
+		set veri_hash_key "" 
+		set veri_mem_key ""
+		set veri_hash_val "" 
+		set veri_score "" 
+		set veri_zadd_key "" 
+		set veri_zadd_mem "" 
         set last_progress [clock seconds]
         while 1 {
             set set_key [randomKeyWithRange 90 127]
@@ -27,129 +28,174 @@ start_server {tags {"maxmemoryb"}} {
             set zadd_mem [randomKeyWithRange 1 127]
 
             if {[catch { r set $set_key $set_val } err]} {
-                if {[string match {*maxmemory*} $err]} {
-                } else {
-                    puts "<erro:> set:<$set_key>:<$set_val>$err"
-                }
-                break
+                puts "<erro:> set:<$set_key>:<$set_val>$err"
+            	incr numerr
             }
             if {[catch { r sadd $sadd_key $sadd_val } err]} {
-                if {[string match {*maxmemory*} $err]} {
-                } else {
-                    puts "<erro:> sadd:<$sadd_key>:<$sadd_val>$err"
-                }
-                break
+                puts "<erro:> sadd:<$sadd_key>:<$sadd_val>$err"
+            	incr numerr
             }
             if {[catch { r hset $hash_key $mem_key $hash_val } err]} {
-                if {[string match {*maxmemory*} $err]} {
-                } else {
-                    puts "<erro:> hset:<$hash_key> <$mem_key>$err"
-                }
-                break
+                puts "<erro:> hset:<$hash_key> <$mem_key>$err"
+            	incr numerr
             }
             set tmp_rank [r zrank $zadd_key $zadd_mem]
             if {$tmp_rank >= 0} {
-	        if {[catch { r zrem $zadd_key $zadd_mem} err]} {
-	            puts "<erro:> zrem <$zadd_key>:<$zadd_mem>$err"
-	            break
-	        }
+	            if {[catch { r zrem $zadd_key $zadd_mem} err]} {
+	                puts "<erro:> zrem <$zadd_key>:<$zadd_mem>$err"
+            	    incr numerr
+	            }
             }
             if {[catch { r zadd $zadd_key $score $zadd_mem} err]} {
-                if {[string match {*maxmemory*} $err]} {
-                } else {
-                    puts "<erro:> zadd <$zadd_key>:<$score>:<$zadd_mem>$err"
-                }
-                break
+                puts "<erro:> zadd <$zadd_key>:<$score>:<$zadd_mem>$err"
+            	incr numerr
             }
             # 用第一次的randomkey做一个初始化操作
             if {[string length $veri_set_key] == 0} {
-		set veri_set_key $set_key
-	        set veri_set_val $set_val
-	        set veri_sadd_key $sadd_key
-		set veri_sadd_val $sadd_val
-		set veri_hash_key $hash_key
-		set veri_mem_key $mem_key
-		set veri_hash_val $hash_val
-		set veri_score $score
-		set veri_zadd_key $zadd_key
-		set veri_zadd_mem $zadd_mem
+				set veri_set_key $set_key
+				set veri_set_val $set_val
+                if {[string length [r smembers $veri_sadd_key]] > 0 } {
+		    		puts "<erro:>smembers  $veri_sadd_key isnot empty"
+            	    incr numerr
+				}
+				set veri_sadd_key $sadd_key
+                if {[string length [r smembers $veri_sadd_key]] == 0 } {
+		    		puts "<erro:>smembers  $veri_sadd_key is empty"
+            	    incr numerr
+				}
+				set veri_sadd_val $sadd_val
+                if {[string length [r hgetall $veri_hash_key]] > 0 } {
+		    		puts "<erro:>hgetall  $veri_hash_key isnot empty"
+            	    incr numerr
+				}
+				set veri_hash_key $hash_key
+                if {[string length [r hgetall $veri_hash_key]] == 0 } {
+		    		puts "<erro:>hgetall  $veri_hash_key is empty"
+            	    incr numerr
+				}
+				set veri_mem_key $mem_key
+				set veri_hash_val $hash_val
+				if { [string length [r zrevrange  $veri_zadd_key 0 -1 withscores]] > 0 } {
+		    		puts "<erro:>hgetall  $veri_zadd_key isnot empty"
+            	    incr numerr
+				}
+				set veri_score $score
+				set veri_zadd_key $zadd_key
+				set veri_zadd_mem $zadd_mem
+				if { [string length [r zrevrange  $veri_zadd_key 0 -1 withscores]] == 0 } {
+		    		puts "<erro:>hgetall  $veri_zadd_key isnot empty"
+            	    incr numerr
+				}
             }
             incr numkeys
             set random_val [randomInt 4000000000]
             if {$random_val % 99 == 0} {
-		set veri_set_key $set_key
-	        set veri_set_val $set_val
-	        set veri_sadd_key $sadd_key
-		set veri_sadd_val $sadd_val
-		set veri_hash_key $hash_key
-		set veri_mem_key $mem_key
-		set veri_hash_val $hash_val
-		set veri_score $score
-		set veri_zadd_key $zadd_key
-		set veri_zadd_mem $zadd_mem
+				set veri_set_key $set_key
+				set veri_set_val $set_val
+				set veri_sadd_key $sadd_key
+				set veri_sadd_val $sadd_val
+				set veri_hash_key $hash_key
+				set veri_mem_key $mem_key
+				set veri_hash_val $hash_val
+				set veri_score $score
+				set veri_zadd_key $zadd_key
+				set veri_zadd_mem $zadd_mem
             }
             if {$random_val % 1000 == 0} {
                 assert_equal $veri_set_val [r get $veri_set_key]
-                assert_equal "string" [r type $veri_set_key]
-                assert_equal "set" [r type $veri_sadd_key]
-                assert_equal "hash" [r type $veri_hash_key]
-                assert_equal "zset" [r type $veri_zadd_key]
-                assert_equal 1 [r exists $veri_set_key]
-                assert_equal 1 [r hexists $veri_hash_key $veri_mem_key]
                 assert_equal 1 [r sismember $veri_sadd_key $veri_sadd_val]
+                if {[string length [r smembers $veri_sadd_key]] == 0 } {
+		    		puts "<erro:>smembers  $veri_sadd_key is empty"
+            	    incr numerr
+				}
                 assert_equal $veri_hash_val [r hget $veri_hash_key $veri_mem_key]
-                set random_key [r randomkey]
-                assert_equal 1 [r exists $random_key]
+                if {[string length [r hgetall $veri_hash_key]] == 0 } {
+		    		puts "<erro:>hgetall  $veri_hash_key is empty"
+            	    incr numerr
+				}
+                assert_equal 1 [r hexists $veri_hash_key $veri_mem_key]
                 set  get_rank [r zrank $veri_zadd_key $veri_zadd_mem]
                 if {[string length $get_rank] == 0} {
-		    puts "<erro:>zrank $veri_zadd_key:$veri_zadd_mem not exist"
-                    break
+		    		puts "<erro:>zrank $veri_zadd_key:$veri_zadd_mem not exist"
+            	    incr numerr
                 }
+                set  get_rev_rank [r zrevrank $veri_zadd_key $veri_zadd_mem]
+                if {[string length $get_rev_rank] == 0} {
+		    		puts "<erro:>zrank $veri_zadd_key:$veri_zadd_mem not exist"
+            	    incr numerr
+                }
+				if { [string length [r zrevrange  $veri_zadd_key 0 -1 withscores]] == 0 } {
+		    		puts "<erro:>hgetall  $veri_zadd_key is empty"
+            	    incr numerr
+				}
+				if { [r zcard $veri_zadd_key] == 0 } {
+		    		puts "<erro:>zcard $veri_zadd_key is empty"
+            	    incr numerr
+				}
             }
             if {$random_val % 1000000 == 0} {
                 set start [clock seconds]
+                set cache_num [r dbsize]
                 LoadFromLdbIfConfig r
                 set num [r dbsize]
                 set end [expr {[clock seconds]-$start}]
-                puts "--------------load from leveldb num:<$num> cost:<$$end>---------------"
+                puts "--------------load from leveldb cache_num:<$cache_num> disk_num:<$num> cost:<$$end>---------------"
                 assert_equal $veri_set_val [r get $veri_set_key]
                 assert_equal 1 [r sismember $veri_sadd_key $veri_sadd_val]
+                if {[string length [r smembers $veri_sadd_key]] == 0 } {
+		    		puts "<erro:>smembers  $veri_sadd_key is empty"
+            	    incr numerr
+				}
                 assert_equal $veri_hash_val [r hget $veri_hash_key $veri_mem_key]
+                if {[string length [r hgetall $veri_hash_key]] == 0 } {
+		    		puts "<erro:>hgetall  $veri_hash_key is empty"
+            	    incr numerr
+				}
                 set  get_rank [r zrank $veri_zadd_key $veri_zadd_mem]
                 if {[string length $get_rank] == 0} {
-		    puts "<erro:>zrank $veri_zadd_key:$veri_zadd_mem not exist"
-                    break
+		    		puts "<erro:>zrank $veri_zadd_key:$veri_zadd_mem not exist"
+            	    incr numerr
                 }
+				if { [string length [r zrevrange  $veri_zadd_key 0 -1 withscores]] == 0 } {
+		    		puts "<erro:>hgetall  $veri_zadd_key is empty"
+            	    incr numerr
+				}
+				if { [r zcard $veri_zadd_key] == 0 } {
+		    		puts "<erro:>zcard $veri_zadd_key is empty"
+            	    incr numerr
+				}
+				if { [r zcount $veri_zadd_key -inf +inf] == 0 } {
+		    		puts "<erro:>zcard $veri_zadd_key is empty"
+            	    incr numerr
+				}
             }
             if {$random_val % 1000000 == 0} {
                 puts "do delete mem operation"
-		r del $veri_set_key
+				r del $veri_set_key
                 r srem $veri_sadd_key $veri_sadd_val
                 r zrem $veri_zadd_key $veri_zadd_mem
                 r hdel $veri_hash_key $veri_mem_key
                 assert_equal "" [r get $veri_set_key]
                 assert_equal 0 [r sismember $veri_sadd_key $veri_sadd_val]
                 assert_equal "" [r hget $veri_hash_key $veri_mem_key]
+                assert_equal 0 [r hexists $veri_hash_key $veri_mem_key]
                 assert_equal "" [r zrank $veri_zadd_key $veri_zadd_mem]
                 LoadFromLdbIfConfig r
                 assert_equal "" [r get $veri_set_key]
                 assert_equal 0 [r sismember $veri_sadd_key $veri_sadd_val]
                 assert_equal "" [r hget $veri_hash_key $veri_mem_key]
                 assert_equal "" [r zrank $veri_zadd_key $veri_zadd_mem]
-		set $veri_set_key ""
-            }
-            if {$random_val % 10000000 == 0} {
-                puts "do delete all db"
-                r flushall
-                assert_equal 0 [r dbsize]
-	        LoadFromLdbIfConfig r
-                assert_equal 0 [r dbsize]
-		set $veri_set_key ""
+				set $veri_set_key ""
             }
             if {$numkeys % 100000 == 0 } {
                 set elapsed [expr {[clock seconds]-$last_progress}]
                 set last_progress [clock seconds]
-        	puts "--------------numkeys<$numkeys> cost time<$elapsed> ts<$last_progress>--------------"
+        		puts "--------------numkeys<$numkeys> cost time<$elapsed> ts<$last_progress>--------------"
+            }
+			# 如果出现了100个错误，该退出查问题了
+            if {$numerr  == 100 } {
+        		puts "<erro>------------error to much, quit--------------ts<[clock seconds]>--------------"
+				break
             }
         }
     }
